67bddf3b4369d6e45cf8c9b525f264f9e612cb39
한권희 <jana32496@gmail.com>
2025-10-25 15:33:01 +0900

    setting: Tailwind CSS 기본 설정 추가 #2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import './components/main.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Test from './pages/Test'
 
 function App() {
   return (
-    <div className="App">
-      hello world
-    </div>
+    <Router>
+      <div className="min-h-screen bg-gray-100 flex flex-col">
+        <main className="flex-grow container mx-auto p-4">
+          <Routes>
+            <Route path="/test" element={<Test />} />
+           
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 }
 
