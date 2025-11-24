c27e7ba8c0b4ebb7c0cb8198371afa915a947b07
ldh0784 <ldhabc58@gmail.com>
2025-11-24 18:47:01 +0900

    fix: change commitlint settings for merge

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,6 +1,8 @@
 // commitlint.config.mjs
 export default {
   extends: ['@commitlint/config-conventional'],
+  ignores: [(message) => message.startsWith("merge")],
 };
+
 
 // npm install --save-dev @commitlint/cli @commitlint/config-conventional
